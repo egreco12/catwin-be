8d062a48879a3d0c9113be637dba29abed061be6
Evan Greco <e.greco12@gmail.com>
2023-03-13 22:12:12 -0700

    Add structure of ESPN golf api responses as TS interfaces

--- src/Tournaments.ts
+++ src/Tournaments.ts
@@ -1,4 +1,24 @@
-interface Tournament {
+class Player {
+  TotalScoreToPar: number;
+  Name: string;
+}
+
+class Tournament {
   Id: number;
   Name: string;
+  Participants: Player[];
 };
+
+const EspnEndpoint = "https://site.web.api.espn.com/apis/v2/scoreboard/header\?sport\=golf\&league\=pga\&region\=us\&lang\=en";
+
+class Tournaments {
+  public static async FetchTournaments(): Promise<Tournament[]> {
+    const response = await fetch(EspnEndpoint);
+    const {data, errors} = await response.json();
+
+    let tournament = new Tournament;
+    return [tournament];
+  }
+}
+
+export {Player, Tournament, Tournaments};
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
+import {Player, Tournament, Tournaments} from "./Tournaments";
 const app = express();
 const port = 8080; // default port to listen
 
-const EspnEndpoint = "https://site.web.api.espn.com/apis/v2/scoreboard/header\?sport\=golf\&league\=pga\&region\=us\&lang\=en";
+
 
 // define a route handler for the default home page
 app.get("/", async (req, res) => {
-  const response = await fetch(EspnEndpoint);
-  const data = await response.json();
-  // tslint:disable-next-line:no-console
+  const data = await Tournaments.FetchTournaments();
   res.send(data);
 });
 
