555ad9ef9bfbf50368b7b6089189ba0ea3e05a7f
Evan Greco <e.greco12@gmail.com>
2023-03-13 21:41:49 -0700

    Use fetch to pull golf leaderboard directly from ESPN

---
+++ src/Tournaments.ts
@@ -0,0 +1,4 @@
+interface Tournament {
+  Id: number;
+  Name: string;
+};
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
+const EspnEndpoint = "https://site.web.api.espn.com/apis/v2/scoreboard/header\?sport\=golf\&league\=pga\&region\=us\&lang\=en";
+
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get("/", async (req, res) => {
+  const response = await fetch(EspnEndpoint);
+  const data = await response.json();
+  // tslint:disable-next-line:no-console
+  res.send(data);
+});
 
 // start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
